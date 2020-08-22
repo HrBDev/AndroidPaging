83e992c230033d8b3a90aff5084fd1657bb92b8c
ftadev <akhlaghi.fatemeh@gmail.com>
2020-08-22 15:22:52 +0430

    + API Methods

--- app/src/main/java/com/ftadev/baman/api/APIService.kt
+++ app/src/main/java/com/ftadev/baman/api/APIService.kt
@@ -1,11 +1,22 @@
 package com.ftadev.baman.api
 
+import com.ftadev.baman.model.Page
+import com.ftadev.baman.model.SampleItem
+import com.ftadev.baman.model.SampleList
+import io.reactivex.Single
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
 
 interface APIService {
+
+    @POST("challenge/list")
+    fun getSampleList(@Body page: Page): Single<SampleList>
+
+    @GET("challenge/getbyid")
+    fun getSampleItem(@Query("page") id: String): Single<SampleItem>
 
     companion object {
         val instance: APIService by lazy {
